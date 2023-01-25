c944c1f947dc79604b6d9cd26a82d63e325a37a7
MOH DEDE FAZRI <fazrithe@gmail.com>
2023-01-25 10:26:18 +0700

    update history

--- resources/views/stock/history.blade.php
+++ resources/views/stock/history.blade.php
@@ -54,18 +54,6 @@
                                     @if($item->opt_answare)
                                     {{ $item->opt_answare }}<br>
                                     {{ $item->answare }}<hr>
-                                    <div class="col-6">
-                                        <a class="btn btn-danger" href="{{ route('request.delete') }}"
-                                        onclick="event.preventDefault();
-                                        document.getElementById('request-delete').submit();">
-                                        Selesai
-                                    </a>
-                                        <form id="request-delete" action="{{ route('request.delete') }}" method="POST" style="display: none;">
-                                        @csrf
-                                            <input type="hidden" name="request_id" value="{{ $item->request_id }}">
-                                        </form>
-                                    </div>
-
                                     @endif
                                 </td>
                               </tr>
